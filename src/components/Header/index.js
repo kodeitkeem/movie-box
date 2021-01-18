2448e40dcc4492573204ad7ea894f304c984ebf7
Akeem Dainty <adainty@businessinsider.com>
2021-01-18 04:53:35 -0500

    adds logout functionality to navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,26 +10,34 @@ import styles from './Header.module.css';
           </Link>
           <nav>
             <ul>
-              <li>
-                <Link to="/login">
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link to="">
-                  Logout
-                </Link>
-              </li>
-              <li>
-                <Link to="/signup">
-                  Signup
-                </Link>
-              </li>
-              <li>
-                <Link to="/dashboard">
-                  My Profile
-                </Link>
-              </li>
+              {
+                props.user ? 
+                <>
+                  <li>
+                    <Link to="" onClick={props.handleLogout}>
+                      Logout
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/dashboard">
+                      My Profile
+                    </Link>
+                  </li>
+                </>
+                :
+                <>
+                  <li>
+                    <Link to="/login">
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/signup">
+                      Signup
+                    </Link>
+                  </li>
+                </>
+              }
             </ul>
           </nav>
         </header>
