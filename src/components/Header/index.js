c184948926562e913420174328774075be667571
Akeem Dainty <adainty@businessinsider.com>
2021-01-20 01:08:32 -0500

    imports multiple bootsrap components to refactor the header navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,51 @@
 import { Link } from 'react-router-dom';
 import logo from '../../../src/logo.svg';
 import styles from './Header.module.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { Nav } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
  function Header(props) {
     return (
         <header className={styles.Header}>
-          <Link to="/">
-            <img src={logo} className={styles.Logo} alt="logo" />
-          </Link>
-          <nav>
-            <ul>
-              {
-                props.user ? 
-                <>
-                  <li>
-                    <Link to="" onClick={props.handleLogout}>
-                      Logout
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/dashboard">
-                      My Profile
-                    </Link>
-                  </li>
-                </>
-                :
-                <>
-                  <li>
-                    <Link to="/login">
-                      Login
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/signup">
-                      Signup
-                    </Link>
-                  </li>
-                </>
-              }
-            </ul>
-          </nav>
+          <Container>
+            <Navbar className={styles.Navbar}>
+              <Navbar.Brand>
+                <Link to="/">
+                  <img src={logo} className={styles.Logo} alt="logo" />
+                </Link>
+              </Navbar.Brand>
+              <Nav className="ml-auto">
+                  {
+                    props.user ? 
+                    <>
+                      <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-info"><i class="fas fa-search"></i>Search</Button>
+                      </Form>
+                      <Nav.Link to="/dashboard">
+                          My Profile
+                      </Nav.Link>
+                      <Nav.Link to="" onClick={props.handleLogout}>
+                          Logout
+                      </Nav.Link>
+                    </>
+                    :
+                    <>
+                      <Nav.Link className={styles.NavLink} to="/login">
+                        Login
+                      </Nav.Link>
+                      <Nav.Link className={styles.NavLink} to="/signup">
+                        Signup
+                      </Nav.Link>
+                    </>
+                  }
+              </Nav>
+            </Navbar>
+          </Container>
         </header>
     )
 }
