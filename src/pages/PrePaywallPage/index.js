a1d91a973479612a0d4b8cb87f7f7c7c69b57629
Akeem Dainty <adainty@businessinsider.com>
2021-01-20 05:51:53 -0500

    adds accordion to pre paywall page and some additional styling

--- src/pages/PrePaywallPage/index.js
+++ src/pages/PrePaywallPage/index.js
@@ -1,17 +1,67 @@
-import { Link } from 'react-router-dom';
-import logo from '../../../src/logo.svg';
 import styles from './PrePaywallPage.module.css';
-import Navbar from 'react-bootstrap/Navbar';
-import  Nav  from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+import Header from '../../components/Header';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import { PrimaryButton } from '../../components/PrimaryButton';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import { Container } from 'react-bootstrap';
+
 
 function PrePaywallPage(props){
+
+    const CardInfo = [
+        {   id: 1,
+            question: "What is Movie Box?",
+            answer: "The dollar menu Netflix"
+        },
+        {
+            id: 2,
+            question: "Isn't there already a Movie Box App?",
+            answer: "No that one is dead and I'm here now, get over it!!"
+        },
+        {
+            id: 3,
+            question: "How much does Movie Box cost?",
+            answer: "We only do yearly subscriptions for $1000/yr"
+        },
+        {
+            id: 4,
+            question: "How do I cancel my subscription?",
+            answer: "We're so sorry but there's no such option"
+        }
+
+    ]
+
     return (
-        <div>
-            Hey
-        </div>
+        <>
+            <div className={styles.Background}>
+            <Header />
+            <Jumbotron className={styles.Jumbotron}>
+                <h1>It's not about what you watch... <br /> it's about how you watch</h1>
+                <p>Watch anytime, anywhere.. but you can never cancel sorry </p>
+                <PrimaryButton /> 
+            </Jumbotron>
+            </div>
+            <div>
+                <Container className={styles.Faqs}>
+                    <h2>Dont know why you'd ask but..</h2>
+                    <h3>Here are some FAQs</h3>
+                    <Accordion defaultActiveKey="0" className={styles.Accordion}>
+                        {CardInfo.map((question)=> (
+                            <Card className={styles.Card}>
+                                <Accordion.Toggle className={styles.CardHeader} as={Card.Header} eventKey={question.id}>
+                                {question.question}
+                                </Accordion.Toggle>
+                                <Accordion.Collapse eventKey={question.id}>
+                                <Card.Body>{question.answer}</Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        ))}
+                    </Accordion>
+                </Container>
+            </div>
+
+        </>
     )
 }
 
