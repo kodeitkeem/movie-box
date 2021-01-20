448be8a7f4e4c130275c84519ead8c862510558b
Akeem Dainty <adainty@businessinsider.com>
2021-01-20 01:06:29 -0500

    imports bootstrap from react, imports PrePaywallPage and comments out useState to prevent api calls in the mean time

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import { useState } from 'react';
 import { getUser,logout } from './services/userService';
+import {useEffect } from 'react';
+import { getContentInfo } from './services/net-api';
 
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import PrePaywallPage from './pages/PrePaywallPage';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {Switch, Route, withRouter, Redirect } from 'react-router-dom';
 
+
 function App(props) {
+
+  /*
+  const [movieData, setMovieData ] = useState({
+    COUNT: "",
+    ITEMS: []
+  })
+
+  async function getAppData(){
+    const data = await getContentInfo();
+    setMovieData(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getAppData();
+  }, []);
+
+*/
   const [ userSate, setUserState ] = useState({
     user: getUser()
   });
@@ -36,8 +60,14 @@ function App(props) {
       <Header handleLogout={handleLogout} user={userSate.user}/>
         <main>
           <Switch>
-            <Route exact path="/" render={props => 
-              <HomePage />
+          <Route exact path="/" render={props => 
+              <PrePaywallPage />
+            } />
+            <Route exact path="/home" render={props => 
+              userSate.user ?
+                <HomePage />
+                :
+                <Redirect to="/login" />
             } />
             <Route exact path="/dashboard" render={props => 
               userSate.user ?
