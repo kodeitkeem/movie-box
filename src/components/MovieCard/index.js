20fde0945165e848e2512eb4bc9857343e77cba9
Akeem Dainty <adainty@businessinsider.com>
2021-01-20 01:12:12 -0500

    adds movie card component

---
+++ src/components/MovieCard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './MovieCard.module.css';
+
+function MovieCard(props){
+    return (
+        <div className={styles.Card}>
+            <h1></h1>
+        </div>
+    )
+}
