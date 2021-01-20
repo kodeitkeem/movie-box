5fff4c86f561b738d625cc316dcd5008de27fe51
Akeem Dainty <adainty@businessinsider.com>
2021-01-20 01:12:50 -0500

    adds pre paywall component

---
+++ src/pages/PrePaywallPage/index.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import logo from '../../../src/logo.svg';
+import styles from './PrePaywallPage.module.css';
+import Navbar from 'react-bootstrap/Navbar';
+import  Nav  from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+function PrePaywallPage(props){
+    return (
+        <div>
+            Hey
+        </div>
+    )
+}
+
+export default PrePaywallPage;
